ddb33bd39e1907908a12b2086940933d8aa67289
Jafrullah <fardeenalam078@gmail.com>
2023-07-24 18:55:14 +0530

    Updates

--- src/component/AllEmails.js
+++
@@ -1,7 +0,0 @@
-const allEmails = ["fardeen@gmail.com",
-"rinku@gmail.com",
-"mintu@gmail.com",
-"shahid@gmail.com",
-"shyam@gmail.com",];
-
-export default allEmails;
--- src/project/MBBS.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function MBBS() {
-  return (
-   <>
-    <h2 style={{color: 'black'}}>I am MMBS page.</h2>
-   </>
-  )
-}
